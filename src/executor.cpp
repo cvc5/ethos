6fe46f42d4b3997823d92566a641f4a5d251dc2f
ajreynol <andrew.j.reynolds@gmail.com>
2024-05-31 16:22:48 -0500

    Missing

---
+++ src/executor.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+ * This file is part of the alfc project.
+ *
+ * Copyright (c) 2023-2024 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ ******************************************************************************/
+
+#include "executor.h"
+#include "state.h"
+
+namespace alfc {
+  
+Executor::Executor(State& s) : d_state(s), d_tc(s.getTypeChecker()) {}
+
+Executor::~Executor() {}
+  
+bool Executor::hasEvaluation(ExprValue* e)
+{
+  return e->isCompiled();
+}
+
+Expr Executor::evaluateProgram(ExprValue* prog,
+                               const std::vector<ExprValue*>& args,
+                               Ctx& ctx)
+{
+  ExprValue * ev = evaluateProgramInternal(args, ctx);
+  if (ev==nullptr)
+  {
+    return d_null;
+  }
+  return Expr(ev);
+}
+
+}
