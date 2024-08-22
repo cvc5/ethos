3508f1cc5891614260a90b50abfe50f77e33d264
ajreynol <andrew.j.reynolds@gmail.com>
2024-08-22 11:55:30 -0500

    Simplify user manual

--- src/parser.cpp
+++ src/parser.cpp
@@ -11,7 +11,7 @@
 namespace ethos {
 
 Parser::Parser(State& s, bool isReference)
-    : d_lex(s.getOptions().d_parseLet),
+    : d_lex(isReference && s.getOptions().d_parseLet),  // only consider let when reference parsing
       d_state(s),
       d_eparser(d_lex, d_state, isReference),
       d_cmdParser(d_lex, d_state, d_eparser, isReference)
