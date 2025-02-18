cvc5/ethos

commit 45098f6ef78b0d85bc0b9374d77585df92c39dcb

Do not evaluate programs on stuck terms (#110)
Modifies our definition so that unapplied literal operators, programs
and oracles are considered "evaluatable".

Modifies the evaluator to not execute programs that have stuck terms.

This leads to better error messages, in particular for users passing
evaluatable functions as higher-order arguments, which is not currently
supported.

After my latest commit on this PR (to undo an oversimplification to the
code), I have verified that the current signature in cvc5 is not
impacted by this change.

src/type_checker.cpp
@@ -934,13 +934,24 @@ Expr TypeChecker::evaluateProgramInternal(
         if (nargs != hchildren.size())
         {
           // TODO: catch this during weak type checking of program bodies
-          Warning() << "*** Bad number of arguments provided in function call to " << hd << std::endl;
+          Warning() << "*** Bad number of arguments provided in function call to " << Expr(hd) << std::endl;
           Warning() << "  Arguments: " << children << std::endl;
           return d_null;
         }
         bool matchSuccess = true;
         for (size_t i=1; i<nargs; i++)
         {
+          // Note we abort here, which changed in Ethos versions >=0.1.2.
+          // The motivation is to disallow unintuitive behaviors of Ethos,
+          // which includes:
+          // - Passing (unapplied) user programs, user oracles or builtin
+          // operators, which we do not support in this current version.
+          // - Passing stuck terms, where we chose to propagate the failure,
+          // e.g. (<program> t) is also stuck if t is stuck.
+          if (children[i]->isEvaluatable())
+          {
+            return d_null;
+          }
           if (!match(hchildren[i], children[i], newCtx))
           {
             matchSuccess = false;

src/cmd_parser.cpp
@@ -748,7 +748,8 @@ bool CmdParser::parseNextCommand()
           Expr rhs = p[1];
           d_eparser.ensureBound(rhs, bvs);
           // TODO: allow variable or default case?
-          for (size_t i = 0, nchildren = pc.getNumChildren(); i < nchildren; i++)
+          for (size_t i = 1, nchildren = pc.getNumChildren(); i < nchildren;
+               i++)
           {
             Expr ecc = pc[i];
             if (ecc.isEvaluatable())

src/expr.cpp
@@ -67,8 +67,9 @@ void ExprValue::computeFlags()
     std::vector<ExprValue*>& children = cur->d_children;
     if (children.empty())
     {
+      bool isEval = (ck == Kind::PROGRAM_CONST || ck == Kind::ORACLE);
       bool isNonGround = (ck==Kind::PARAM);
-      cur->setFlag(Flag::IS_EVAL, false);
+      cur->setFlag(Flag::IS_EVAL, isEval);
       cur->setFlag(Flag::IS_NON_GROUND, isNonGround);
       visit.pop_back();
     }
@@ -86,34 +87,15 @@ void ExprValue::computeFlags()
     else
     {
       visit.pop_back();
-      if (ck==Kind::APPLY)
+      if (isLiteralOp(ck))
       {
-        Kind cck = children[0]->getKind();
-        if (cck==Kind::PROGRAM_CONST || cck==Kind::ORACLE)
-        {
-          cur->setFlag(Flag::IS_PROG_EVAL, true);
-          cur->setFlag(Flag::IS_EVAL, true);
-        }
-      }
-      else if (isLiteralOp(ck))
-      {
-        // requires type and literal operator kinds evaluate
+        // literal operator kinds are evaluatable
         cur->setFlag(Flag::IS_EVAL, true);
       }
+      // flags are a union of the flags of the children
       for (ExprValue* c : children)
       {
-        if (c->getFlag(Flag::IS_NON_GROUND))
-        {
-          cur->setFlag(Flag::IS_NON_GROUND, true);
-        }
-        if (c->getFlag(Flag::IS_EVAL))
-        {
-          cur->setFlag(Flag::IS_EVAL, true);
-        }
-        if (c->getFlag(Flag::IS_PROG_EVAL))
-        {
-          cur->setFlag(Flag::IS_PROG_EVAL, true);
-        }
+        cur->d_flags |= static_cast<uint8_t>(c->d_flags);
       }
     }
   }
@@ -129,18 +111,6 @@ bool ExprValue::isGround()
 {
   computeFlags();
   return !getFlag(ExprValue::Flag::IS_NON_GROUND);
-}
-
-bool ExprValue::isProgEvaluatable()
-{
-  computeFlags();
-  return getFlag(ExprValue::Flag::IS_PROG_EVAL);
-}
-
-bool ExprValue::isCompiled()
-{
-  // this is set manually
-  return getFlag(ExprValue::Flag::IS_COMPILED);
 }
 
 void ExprValue::dec()
@@ -188,12 +158,6 @@ Expr::~Expr()
 bool Expr::isNull() const { return d_value->isNull(); }
 bool Expr::isEvaluatable() const { return d_value->isEvaluatable(); }
 bool Expr::isGround() const { return d_value->isGround(); }
-bool Expr::isProgEvaluatable() const { return d_value->isProgEvaluatable(); }
-bool Expr::isCompiled() const { return d_value->isCompiled(); }
-void Expr::setCompiled()
-{
-  return d_value->setFlag(ExprValue::Flag::IS_COMPILED, true);
-}
 std::string Expr::getSymbol() const
 {
   const Literal * l = d_value->asLiteral();

src/expr.h
@@ -57,24 +57,28 @@ class ExprValue
   bool isEvaluatable();
   /** Has variable */
   bool isGround();
-  /** Has program variable */
-  bool isProgEvaluatable();
-  /** Is part of compiled code */
-  bool isCompiled();
  protected:
   /** The kind */
   Kind d_kind;
   /** The children of this expression */
   std::vector<ExprValue*> d_children;
-  /** flags */
+  /**
+   * Flags, indicating properties of the term.
+   */
   enum class Flag
   {
     NONE = 0,
+    /** Have we computed the flags for this term? */
     IS_FLAGS_COMPUTED = (1 << 0),
+    /**
+     * Is the term "evaluatable", i.e. contains a literal op, program or oracle
+     * as a subterm?
+     */
     IS_EVAL = (1 << 1),
-    IS_PROG_EVAL = (1 << 2),
-    IS_NON_GROUND = (1 << 3),
-    IS_COMPILED = (1 << 4)
+    /**
+     * Is the term non-ground, i.e. contains a parameter as a subterm?
+     */
+    IS_NON_GROUND = (1 << 2),
   };
   char d_flags;
   /** */
@@ -147,12 +151,6 @@ class Expr
   bool isEvaluatable() const;
   /** Has variable */
   bool isGround() const;
-  /** Has program variable */
-  bool isProgEvaluatable() const;
-  /** Is part of compiled code */
-  bool isCompiled() const;
-  /** Set compiled */
-  void setCompiled();
   /** Get symbol */
   std::string getSymbol() const;
   /** Get underlying value */

tests/CMakeLists.txt
@@ -133,6 +133,8 @@ set(ethos_test_file_list
     simul-overload.eo
     bang-lex.eo
     segfault-98.eo
+    eo-add-non-first-class.eo
+    unstuck-eval-bv-example.eo
     param-dt-parse.eo
     param-dt-parse-amb-fun.eo
     datatypes-split-rule-param.eo

tests/Strings-programs.eo
@@ -84,10 +84,10 @@
 (define string_rev ((U Type) (rev Bool) (t (Seq U))) (eo::ite rev ($string_rev t) t))
 
 ; Returns true if the length of s evaluates to one, false otherwise.
-(define check_length_one ((s String)) (check_true (eo::is_eq (eo::len s) 1)))
+(define check_length_one ((s String)) (eo::is_eq (eo::len s) 1))
 
 ; Returns true if the length of s evaluates to greater than one, false otherwise.
-(define check_length_gt_one ((s String)) (check_true (eo::is_neg (eo::add 1 (eo::neg (eo::len s))))))
+(define check_length_gt_one ((s String)) (eo::is_eq (eo::is_neg (eo::add 1 (eo::neg (eo::len s)))) true))
 
 ; Flatten constants in str.++ application s. Notice that the rewritten form
 ; of strings in cvc5 are such that constants are grouped into constants of

tests/eo-add-non-first-class.eo
@@ -1,0 +1,34 @@
+(declare-type Int ())
+(declare-consts <numeral> Int)
+
+(declare-const g (-> Int Int Int))
+
+(program dummy ((f (-> Int Int Int)) (n1 Int) (n2 Int))
+    (Int Int (-> Int Int Int)) Int
+
+    (((dummy n1 n2 f) (f n1 n2)))
+)
+
+; this should work
+(define tmp1 () (dummy 1 1 g))
+(declare-const c1 (eo::requires tmp1 (g 1 1) Int))
+(define test1 () c1 :type Int)
+
+
+(program foo ((n1 Int) (n2 Int))
+    (Int Int) Int
+
+    (((foo n1 n2) n1))
+)
+
+
+; this should not work
+;(define tmp2 () (dummy 1 1 foo))
+;(declare-const c2 (eo::requires tmp2 1 Int))
+;(define test1 () c2 :type Int)
+
+
+; this should not work
+;(define tmp3 () (dummy 1 1 eo::add))
+;(declare-const c3 (eo::requires tmp3 2 Int))
+;(define test2 () c3 :type Int)

tests/unstuck-eval-bv-example.eo
@@ -1,0 +1,52 @@
+; ethos gave a spurious error on this example in cvc5 regressions when the
+; definition of evaluatable was oversimplified to not check literalOp kinds.
+
+(declare-type Int ())
+(declare-consts <numeral> Int)
+
+(declare-const = (-> (! Type :var A :implicit) A A Bool))
+(declare-const < (-> Int Int Bool))
+(declare-const - (-> Int Int Int))
+
+; note: We do not currently check that the index of this sort is positive.
+(declare-const BitVec (-> Int Type))
+(declare-consts <binary> (BitVec (eo::len eo::self)))
+
+; bvsize
+(declare-const @bvsize (-> (! Int :var m :implicit) (BitVec m) Int))
+
+; define: @bv_empty
+; return: The empty bitvector.
+(define @bv_empty () (eo::to_bin 0 0))
+
+(declare-const concat (->
+  (! Int :var n :implicit)
+  (! Int :var m :implicit)
+  (BitVec n)
+  (BitVec m)
+  (BitVec (eo::add n m))) :right-assoc-nil @bv_empty
+)
+
+(declare-const extract (->
+  (! Int :var n :implicit)
+  (! Int :var h)
+  (! Int :var l)
+  (BitVec n)
+  (BitVec (eo::add h (eo::neg l) 1))
+  )
+)
+(declare-rule bv-extract-concat-4 ((@n0 Int) (@n1 Int) (@n2 Int) (x1 (BitVec @n0)) (y1 (BitVec @n1)) (xs1 (BitVec @n2) :list) (i1 Int) (j1 Int))
+  :premises ((= (< j1 (- (@bvsize (concat x1 xs1 y1)) (@bvsize x1))) true))
+  :args (x1 y1 xs1 i1 j1)
+  :conclusion (= (extract j1 i1 (concat x1 xs1 y1)) (extract j1 i1 (concat xs1 y1)))
+)
+
+(declare-const a (_ BitVec 4))
+(declare-const b (_ BitVec 4))
+(declare-const c (_ BitVec 4))
+(declare-const d (_ BitVec 4))
+
+(assume @p30210 (= (< 2 (- (@bvsize (concat a b c d)) (@bvsize a))) true))
+(step @p30211 :rule bv-extract-concat-4 :premises (@p30210) :args (a d (concat b c) 0 2))
+
+

commit afa7ba19b433214d973873b90eaa2f8e171afde9

Add --stats-all (#119)
This adds a new class of statistics that counts the number of calls to
programs, which can enable more fine grained profiling.

This also corrects a minor inconsistency in oracles/programs introduced
in https://github.com/cvc5/ethos/pull/110, where the check for "stuck"
terms should be applied uniformly for programs and oracles.

Example output:

```
========================================================================
mkExprCount = 793
newExprCount = 700
deleteExprCount = 69
symCount = 226
litCount = 7
time = 23904
====================================================================================
                                  Rule  #      t                t/#       #mkExpr   
====================================================================================
                          arith_sum_ub: 1      1565 (6.5%)      1565      46        
                        arith_mult_neg: 1      753 (3.2%)       753       25        
                        arith_mult_pos: 1      262 (1.1%)       262       22        
====================================================================================
                               Program  #      
====================================================================================
                         is_arith_type: 18
                  arith_typeunion_nary: 13
                       mk_arith_sum_ub: 4
                         arith_rel_sum: 3
                  mk_arith_sum_ub_step: 3
                         arith_rel_inv: 1
                     mk_arith_mult_pos: 1
                     mk_arith_mult_neg: 1

src/expr_parser.cpp
@@ -596,7 +596,7 @@ Expr ExprParser::parseExpr()
               // make the program variable, whose type is abstract
               Expr ftype = d_state.mkFunctionType(fargTypes, atype, false);
               std::stringstream pvname;
-              pvname << "_match_" << hd;
+              pvname << "eo::match_" << hd;
               Expr pv = d_state.mkSymbol(Kind::PROGRAM_CONST, pvname.str(), ftype);
               // process the cases
               std::vector<Expr> cases;

src/main.cpp
@@ -21,14 +21,13 @@ using namespace ethos;
 int main( int argc, char* argv[] )
 {
   Options opts;
-  Stats stats;
-  State s(opts, stats);
-  Plugin* plugin = nullptr;
   // read the options
   size_t i = 1;
   std::string file;
   bool readFile = false;
   size_t nargs = static_cast<size_t>(argc);
+  // the list of includes and whether they were an include or reference
+  std::vector<std::pair<std::string, bool>> includes;
   while (i<nargs)
   {
     std::string arg(argv[i]);
@@ -50,14 +49,10 @@ int main( int argc, char* argv[] )
     bool isInclude = (arg.compare(0, 10, "--include=") == 0);
     if (isInclude || arg.compare(0, 12, "--reference=") == 0)
     {
+      // defer the inclusion until the options are finalized
       size_t first = arg.find_first_of("=");
       std::string file = arg.substr(first + 1);
-      // cannot provide reference
-      Expr refNf;
-      if (!s.includeFile(file, isInclude, !isInclude, refNf))
-      {
-        EO_FATAL() << "Error: cannot include file " << file;
-      }
+      includes.emplace_back(file, isInclude);
       continue;
     }
     if (arg == "--help")
@@ -75,6 +70,7 @@ int main( int argc, char* argv[] )
       out << "      --reference=X: includes the file specified by X as a reference file." << std::endl;
       out << "      --show-config: displays the build information for this binary." << std::endl;
       out << "            --stats: enables detailed statistics." << std::endl;
+      out << "        --stats-all: enables all available statistics." << std::endl;
       out << "    --stats-compact: print statistics in a compact format." << std::endl;
       out << "           -t <tag>: enables the given trace tag (requires debug build)." << std::endl;
       out << "                 -v: verbose mode, enable all standard trace messages (requires debug build)." << std::endl;
@@ -147,6 +143,21 @@ int main( int argc, char* argv[] )
       EO_FATAL() << "Error: mulitple files specified, \"" << file << "\" and \"" << arg << "\"";
     }
   }
+  // options are finalized, now initialize the state and run the includes
+  Stats stats;
+  State s(opts, stats);
+  Plugin* plugin = nullptr;
+  for (size_t i=0, nincludes=includes.size(); i<nincludes; i++)
+  {
+    std::string file = includes[i].first;
+    bool isInclude = includes[i].second;
+    // cannot provide reference
+    Expr refNf;
+    if (!s.includeFile(file, isInclude, !isInclude, refNf))
+    {
+      EO_FATAL() << "Error: cannot include file " << file;
+    }
+  }
   // NOTE: initialization of plugin goes here
   if (plugin!=nullptr)
   {
@@ -202,7 +213,7 @@ int main( int argc, char* argv[] )
   }
   if (opts.d_stats)
   {
-    std::cout << stats.toString(s, opts.d_statsCompact);
+    std::cout << stats.toString(s, opts.d_statsCompact, opts.d_statsAll);
   }
   // exit immediately, which avoids deleting all expressions which can take time
   exit(0);

src/state.cpp
@@ -22,6 +22,7 @@ Options::Options()
   d_parseLet = true;
   d_printLet = false;
   d_stats = false;
+  d_statsAll = false;
   d_statsCompact = false;
   d_ruleSymTable = true;
   d_normalizeDecimal = true;
@@ -48,13 +49,16 @@ bool Options::setOption(const std::string& key, bool val)
   {
     d_stats = val;
   }
+  else if (key == "stats-all")
+  {
+    // also implies stats are enabled.
+    d_stats = val ? true : d_stats;
+    d_statsAll = val;
+  }
   else if (key == "stats-compact")
   {
-    if (val)
-    {
-      // also implies stats are enabled.
-      d_stats = val;
-    }
+    // also implies stats are enabled.
+    d_stats = val ? true : d_stats;
     d_statsCompact = val;
   }
   else if (key == "rule-sym-table")
@@ -86,9 +90,9 @@ State::State(Options& opts, Stats& stats)
     : d_hashCounter(0),
       d_hasReference(false),
       d_inGarbageCollection(false),
-      d_tc(*this, opts),
       d_opts(opts),
       d_stats(stats),
+      d_tc(*this, opts),
       d_plugin(nullptr)
 {
   ExprValue::d_state = this;

src/state.h
@@ -38,6 +38,7 @@ class Options
   /** 'let' is lexed as the SMT-LIB syntax for a dag term specified by a let */
   bool d_parseLet;
   bool d_stats;
+  bool d_statsAll;
   bool d_statsCompact;
   bool d_ruleSymTable;
   bool d_normalizeDecimal;
@@ -332,12 +333,12 @@ class State
   /** Are we in garbage collection? */
   bool d_inGarbageCollection;
   //--------------------- utilities
-  /** Type checker */
-  TypeChecker d_tc;
   /** Options */
   Options& d_opts;
   /** Stats */
   Stats& d_stats;
+  /** Type checker */
+  TypeChecker d_tc;
   /** Plugin, if using one */
   Plugin* d_plugin;
 };

src/stats.cpp
@@ -43,12 +43,10 @@ std::string RuleStat::toString(std::time_t totalTime) const
 {
   std::stringstream ss;
   std::stringstream st;
+  ss << std::left << std::setw(7) << d_count;
   double pct = static_cast<double>(100*d_time)/static_cast<double>(totalTime);
   st << d_time << " (" << std::fixed << std::setprecision(1) << pct << "%)";
   ss << std::left << std::setw(17) << st.str();
-  std::stringstream sc;
-  sc << d_count;
-  ss << std::left << std::setw(7) << sc.str();
   // time per rule
   double timePerRule = static_cast<double>(d_time)/static_cast<double>(d_count);
   std::stringstream sp;
@@ -65,6 +63,9 @@ Stats::Stats() : d_mkExprCount(0), d_exprCount(0), d_deleteExprCount(0), d_symCo
   d_startTime = getCurrentTime();
 }
 
+/**
+ * Either sorts by time or by counts
+ */
 struct SortRuleTime
 {
   SortRuleTime(const std::map<const ExprValue*, RuleStat>& rs) : d_rstats(rs)
@@ -79,16 +80,20 @@ struct SortRuleTime
     std::map<const ExprValue*, RuleStat>::const_iterator itrj;
     itrj = d_rstats.find(j);
     Assert (itrj!=d_rstats.end());
+    if (itri->second.d_time==itrj->second.d_time)
+    {
+      return itri->second.d_count>itrj->second.d_count;
+    }
     return itri->second.d_time>itrj->second.d_time;
   }
 };
 
-std::string Stats::toString(State& s, bool compact) const
+std::string Stats::toString(State& s, bool compact, bool all) const
 {
   std::stringstream ss;
   if (!compact)
   {
-    ss << "========================================================================" << std::endl;
+    ss << "====================================================================================" << std::endl;
   }
   ss << "mkExprCount = " << d_mkExprCount << std::endl;
   ss << "newExprCount = " << d_exprCount << std::endl;
@@ -97,40 +102,56 @@ std::string Stats::toString(State& s, bool compact) const
   ss << "litCount = " << d_litCount << std::endl;
   std::time_t totalTime = (getCurrentTime()-d_startTime);
   ss << "time = " << totalTime << std::endl;
-  if (!d_rstats.empty())
+  size_t imax = all ? 2 : 1;
+  for (size_t i=0; i<imax; i++)
   {
+    const std::map<const ExprValue*, RuleStat>& rs = i==0 ? d_rstats : d_pstats;
+    if (rs.empty())
+    {
+      continue;
+    }
     if (!compact)
     {
-      ss << "========================================================================" << std::endl;
-      ss << std::right << std::setw(28) << "Rule  ";
-      ss << std::left << std::setw(17) << "t";
+      ss << "====================================================================================" << std::endl;
+      ss << std::right << std::setw(40) << (i==0 ? "Rule  " : "Program  ");
       ss << std::left << std::setw(7) << "#";
-      ss << std::left << std::setw(10) << "t/#";
-      ss << std::left << std::setw(10) << "#mkExpr";
+      if (i==0)
+      {
+        ss << std::left << std::setw(17) << "t";
+        ss << std::left << std::setw(10) << "t/#";
+        ss << std::left << std::setw(10) << "#mkExpr";
+      }
       ss << std::endl;
-      ss << "========================================================================" << std::endl;
+      ss << "====================================================================================" << std::endl;
     }
     // display stats for each rule
     std::vector<const ExprValue*> sortedStats;
-    for (const std::pair<const ExprValue* const, RuleStat>& r : d_rstats)
+    for (const std::pair<const ExprValue* const, RuleStat>& r : rs)
     {
+      // might be an internal match program, in which case, skip
+      Expr sym(r.first);
+      std::string symbol = sym.getSymbol();
+      if (symbol.compare(0, 4, "eo::")==0)
+      {
+        continue;
+      }
       sortedStats.push_back(r.first);
     }
     // sort based on time
-    SortRuleTime srt(d_rstats);
+    SortRuleTime srt(rs);
     std::sort(sortedStats.begin(), sortedStats.end(), srt);    
     std::map<const ExprValue*, RuleStat>::const_iterator itr;
+    std::stringstream ssCount;
     std::stringstream ssCheck;
     std::stringstream ssMkExpr;
     bool firstTime = true;
     for (const ExprValue* e : sortedStats)
     {
-      itr = d_rstats.find(e);
-      Assert (itr!=d_rstats.end());
+      itr = rs.find(e);
+      Assert (itr!=rs.end());
       const RuleStat& rs = itr->second;
-      Assert (e->getKind()==Kind::PROOF_RULE);
-      std::stringstream sss;
-      sss << Expr(e);
+      std::stringstream ssExpr;
+      ssExpr << Expr(e);
       if (compact)
       {
         if (firstTime)
@@ -139,22 +160,36 @@ std::string Stats::toString(State& s, bool compact) const
         }
         else
         {
+          ssCount << ", ";
           ssCheck << ", ";
           ssMkExpr << ", ";
         }
-        ssCheck << sss.str() << ": " << rs.d_time;
-        ssMkExpr << sss.str() << ": " << rs.d_mkExprCount;
+        ssCount << ssExpr.str() << ": " << rs.d_count;
+        ssCheck << ssExpr.str() << ": " << rs.d_time;
+        ssMkExpr << ssExpr.str() << ": " << rs.d_mkExprCount;
       }
       else
       {
-        sss << ": ";
-        ss << std::right << std::setw(28) << sss.str() << rs.toString(totalTime) << std::endl;
+        ssExpr << ": ";
+        ss << std::right << std::setw(40) << ssExpr.str();
+        if (i==0)
+        {
+          ss << rs.toString(totalTime) << std::endl;
+        }
+        else
+        {
+          ss << rs.d_count << std::endl;
+        }
       }
     }
     if (compact)
     {
-      ss << "checkTime = { " << ssCheck.str() << " }" << std::endl;
-      ss << "mkExpr = { " << ssMkExpr.str() << " }" << std::endl;
+      ss << (i==0 ? "rule" : "prog") << "Count = { " << ssCount.str() << " }" << std::endl;
+      if (i==0)
+      {
+        ss << "checkTime = { " << ssCheck.str() << " }" << std::endl;
+        ss << "mkExpr = { " << ssMkExpr.str() << " }" << std::endl;
+      }
     }
   }
   return ss.str();

src/stats.h
@@ -46,7 +46,8 @@ public:
   size_t d_litCount;
   std::time_t d_startTime;
   std::map<const ExprValue*, RuleStat> d_rstats;
-  std::string toString(State& s, bool compact) const;
+  std::map<const ExprValue*, RuleStat> d_pstats;
+  std::string toString(State& s, bool compact, bool all) const;
 
   static std::time_t getCurrentTime();
 };

src/type_checker.cpp
@@ -24,7 +24,7 @@
 
 namespace ethos {
 
-TypeChecker::TypeChecker(State& s, Options& opts) : d_state(s), d_plugin(nullptr)
+TypeChecker::TypeChecker(State& s, Options& opts) : d_state(s), d_plugin(nullptr), d_sts(s.getStats())
 {
   std::set<Kind> literalKinds = { Kind::BOOLEAN, Kind::NUMERAL, Kind::RATIONAL, Kind::BINARY, Kind::STRING, Kind::DECIMAL, Kind::HEXADECIMAL };
   // initialize literal kinds 
@@ -32,6 +32,7 @@ TypeChecker::TypeChecker(State& s, Options& opts) : d_state(s), d_plugin(nullptr
   {
     d_literalTypeRules[k] = d_null;
   }
+  d_statsEnabled = opts.d_stats;
 }
 
 TypeChecker::~TypeChecker()
@@ -908,6 +909,21 @@ Expr TypeChecker::evaluateProgramInternal(
     // do not evaluate on non-ground
     return d_null;
   }
+  // Note we abort here, which changed in Ethos versions >=0.1.2.
+  // The motivation is to disallow unintuitive behaviors of Ethos,
+  // which includes:
+  // - Passing (unapplied) user programs, user oracles or builtin
+  // operators, which we do not support in this current version.
+  // - Passing stuck terms, where we chose to propagate the failure,
+  // e.g. (<program> t) is also stuck if t is stuck.
+  size_t nargs = children.size();
+  for (size_t j=1; j<nargs; j++)
+  {
+    if (children[j]->isEvaluatable())
+    {
+      return d_null;
+    }
+  }
   ExprValue* hd = children[0];
   Kind hk = hd->getKind();
   if (hk==Kind::PROGRAM_CONST)
@@ -917,8 +933,12 @@ Expr TypeChecker::evaluateProgramInternal(
       Trace("type_checker") << "RUN program " << children << std::endl;
       return d_plugin->evaluateProgram(hd, children, newCtx);
     }
-    size_t nargs = children.size();
     Expr prog = d_state.getProgram(hd);
+    if (d_statsEnabled)
+    {
+      RuleStat * ps = &d_sts.d_pstats[hd];
+      ps->d_count++;
+    }
     Assert (!prog.isNull());
     if (!prog.isNull())
     {
@@ -939,20 +959,9 @@ Expr TypeChecker::evaluateProgramInternal(
           return d_null;
         }
         bool matchSuccess = true;
-        for (size_t i=1; i<nargs; i++)
+        for (size_t j = 1; j<nargs; j++)
         {
-          // Note we abort here, which changed in Ethos versions >=0.1.2.
-          // The motivation is to disallow unintuitive behaviors of Ethos,
-          // which includes:
-          // - Passing (unapplied) user programs, user oracles or builtin
-          // operators, which we do not support in this current version.
-          // - Passing stuck terms, where we chose to propagate the failure,
-          // e.g. (<program> t) is also stuck if t is stuck.
-          if (children[i]->isEvaluatable())
-          {
-            return d_null;
-          }
-          if (!match(hchildren[i], children[i], newCtx))
+          if (!match(hchildren[j], children[j], newCtx))
           {
             matchSuccess = false;
             break;
@@ -978,10 +987,9 @@ Expr TypeChecker::evaluateProgramInternal(
       return d_null;
     }
     int retVal;
-#if 1
     std::stringstream call_content;
     call_content << "(" << std::endl;
-    for (size_t i = 1, nchildren = children.size(); i < nchildren; i++)
+    for (size_t i = 1; i < nargs; i++)
     {
       call_content << Expr(children[i]) << std::endl;
     }
@@ -992,20 +1000,6 @@ Expr TypeChecker::evaluateProgramInternal(
     Trace("oracles") << "```" << std::endl;
     std::stringstream response;
     retVal = run(ocmd, call_content.str(), response);
-#else
-    std::stringstream call;
-    call << ocmd;
-    for (size_t i = 1, nchildren = children.size(); i < nchildren; i++)
-    {
-      call << " " << Expr(children[i]);
-    }
-    Trace("oracles") << "Call oracle " << ocmd << " with content:" << std::endl;
-    Trace("oracles") << "```" << std::endl;
-    Trace("oracles") << call.str() << std::endl;
-    Trace("oracles") << "```" << std::endl;
-    std::stringstream response;
-    retVal = runFile(call.str(), response);
-#endif
     if (retVal!=0)
     {
       Trace("oracles") << "...failed to run" << std::endl;

src/type_checker.h
@@ -18,6 +18,7 @@
 namespace ethos {
 
 class State;
+class Stats;
 class Options;
 class Plugin;
 
@@ -123,6 +124,10 @@ class TypeChecker
   /** The null expression */
   Expr d_null;
   Expr d_negOne;
+  /** Stats enabled? */
+  bool d_statsEnabled;
+  /** Reference to the stats */
+  Stats& d_sts;
 };
 
 }  // namespace ethos