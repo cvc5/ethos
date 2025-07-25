d7192e91a24555184e29b69d806bebda7217827d
ajreynol <andrew.j.reynolds@gmail.com>
2025-07-25 07:07:57 -0500

    Fix

--- plugins/model_smt/model_smt.cpp
+++ plugins/model_smt/model_smt.cpp
@@ -173,7 +173,7 @@ ModelSmt::ModelSmt(State& s) : StdPlugin(s)
   addReduceSym("set.empty", {kT}, "($smtx_set_empty x1)");
   addRecReduceSym("set.singleton", {kT}, "($smtx_set_singleton e1)");
   addRecReduceSym("set.inter", {kT, kT}, "($smtx_set_inter e1 e2)");
-  addRecReduceSym("set.diff", {kT, kT}, "($smtx_set_diff e1 e2)");
+  addRecReduceSym("set.minus", {kT, kT}, "($smtx_set_minus e1 e2)");
   addRecReduceSym("set.union", {kT, kT}, "($smtx_set_union e1 e2)");
   addRecReduceSym("set.member", {kT, kT}, "($smtx_select e2 e1)");
   addTermReduceSym("set.subset", {kT, kT}, "(= (set.inter x1 x2) x1)");
