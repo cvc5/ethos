f69def2dfdbf4e44c1de89798bf4fbf88a8863f2
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2025-10-28 14:08:11 -0500

    Forbid evaluation in types of parameters (#173)
    This forbids examples such as the following:
    ```
    (declare-parameterized-const foo1 
      ((T Type) (U Type) (F Bool) (c (eo::ite F T U)))
      Type)
    
    (program foo2 ((T Type) (U Type) (F Bool) (t T))  
      :signature ((eo::ite F T U)) Type
      (
      ((foo2 t) Type)
      )
    )
    ```
    The intuitive reason is that argument types is analogous to patterns in
    program evaluation, where evaluation is forbidden.
    
    Notice that these restrictions forbid the combination of quote and
    evaluatable types. The restriction does *not* forbid the use of
    evaluation in any function argument type. In particular, it is important
    to note that the following example is still permitted which is
    equivalent to the first example:
    ```
    (declare-parameterized-const foo1b 
      ((T Type) (U Type) (F Bool)) 
      (-> (eo::ite F T U) Type))  ; still allowed
    ```
    
    On the other hand, this example is disallowed and has no equivalent
    after this PR:
    ```
    (declare-const Set (-> Type Type))
    (declare-parameterized-const foo3
      ((T Type) (U Type) (F Bool) (c (eo::ite F T U)) (w Type)) 
      (Set w))
    ```

--- src/expr_parser.cpp
+++ src/expr_parser.cpp
@@ -467,7 +467,7 @@ Expr ExprParser::parseExpr()
   return ret;
 }
 
-Expr ExprParser::parseType(bool allowQuoteArg)
+Expr ExprParser::parseType(bool allowQuoteArg, bool allowEval)
 {
   if (allowQuoteArg)
   {
@@ -501,13 +501,25 @@ Expr ExprParser::parseType(bool allowQuoteArg)
   // ensure it is a type
   typeCheck(e, d_state.mkType());
   // should not contain stuck term
-  if (e.isGround() && e.isEvaluatable())
+  if (e.isEvaluatable())
   {
-    std::stringstream msg;
-    msg << "Parsed type has an unevalated term:" << std::endl;
-    msg << "Type: " << e << std::endl;
-    d_lex.parseError(msg.str());
+    if (e.isGround())
+    {
+      std::stringstream msg;
+      msg << "Parsed type has an unevalated term:" << std::endl;
+      msg << "Type: " << e << std::endl;
+      d_lex.parseError(msg.str());
+    }
+    else if (!allowEval)
+    {
+      std::stringstream msg;
+      msg << "Parsed type cannot contain evaluation in this context:"
+          << std::endl;
+      msg << "Type: " << e << std::endl;
+      d_lex.parseError(msg.str());
+    }
   }
+
   return e;
 }
 
@@ -543,7 +555,8 @@ std::vector<Expr> ExprParser::parseTypeList(bool allowQuoteArg)
   while (tok != Token::RPAREN)
   {
     d_lex.reinsertToken(tok);
-    Expr t = parseType(allowQuoteArg);
+    // never allow evaluation
+    Expr t = parseType(allowQuoteArg, false);
     terms.push_back(t);
     tok = d_lex.nextToken();
   }
@@ -617,7 +630,8 @@ std::vector<Expr> ExprParser::parseAndBindSortedVarList(
   while (d_lex.eatTokenChoice(Token::LPAREN, Token::RPAREN))
   {
     name = parseSymbol();
-    t = parseType();
+    // do not allow quote or evaluation
+    t = parseType(false, false);
     Expr v;
     bool isImplicit = false;
     if (k == Kind::NONE)
