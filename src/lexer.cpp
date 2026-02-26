57e08380cff3c727a2968ec728a3de622894dbf8
ajreynol <andrew.j.reynolds@gmail.com>
2026-02-26 17:28:11 -0600

    Format

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -26,8 +26,13 @@ std::ostream& operator<<(std::ostream& o, const Span& l)
 }
 
 Lexer::Lexer(bool lexLet, bool lexType)
-    : d_lexLet(lexLet), d_lexType(lexType), d_isInteractive(false), d_bufferPos(0), d_bufferEnd(0),
-      d_peekedChar(false), d_chPeeked(0)
+    : d_lexLet(lexLet),
+      d_lexType(lexType),
+      d_isInteractive(false),
+      d_bufferPos(0),
+      d_bufferEnd(0),
+      d_peekedChar(false),
+      d_chPeeked(0)
 {
   for (int32_t ch = 'a'; ch <= 'z'; ++ch)
   {
@@ -457,7 +462,8 @@ Token Lexer::tokenizeCurrentSymbol() const
       }
       break;
     case 'T':
-      if (d_lexType && d_token.size() == 4 && d_token[1] == 'y' && d_token[2] == 'p' && d_token[3] == 'e')
+      if (d_lexType && d_token.size() == 4 && d_token[1] == 'y'
+          && d_token[2] == 'p' && d_token[3] == 'e')
       {
         return Token::TYPE;
       }
