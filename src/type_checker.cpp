d2660adbc9410271f8330ce650b974714d7f1b4e
ajreynol <andrew.j.reynolds@gmail.com>
2026-04-30 10:54:17 -0500

    Simplify type checker since quote only takes parameters

--- src/type_checker.cpp
+++ src/type_checker.cpp
@@ -382,7 +382,6 @@ Expr TypeChecker::getTypeAppInternal(std::vector<ExprValue*>& children,
     // if the argument is (Quote t), we match on its argument,
     // which along with how ctypes[i] is the argument itself, has the effect
     // of an implicit upcast.
-    bool isQuote = false;
     bool typeSuccess = true;
     ExprValue* child = children[i];
     if (hdt->getKind() == Kind::QUOTE_TYPE)
@@ -399,36 +398,33 @@ Expr TypeChecker::getTypeAppInternal(std::vector<ExprValue*>& children,
         hdt = cte;
         child = ct;
       }
-      // if the above check was success, we quote
-      if (typeSuccess)
+      else
       {
+        // otherwise we have a function of type (-> (Quote x) T) taking
+        // argument t. Since (Quote x) is only constructed for parameters x,
+        // we know that x is a parameter of the same type as t. We don't need
+        // to match, we simply update the context.
         hdt = hdt->d_children[0];
-        isQuote = true;
+        Assert(hdt->getKind()==Kind::PARAM);
+        Assert (ctx.find(hdt)==ctx.end());
+        ctx[hdt] = child;
       }
     }
     else
     {
       child = d_state.lookupType(child);
       Assert(child != nullptr);
+      typeSuccess = match(hdt, child, ctx, visited);
     }
-    if (!typeSuccess || !match(hdt, child, ctx, visited))
+    if (!typeSuccess)
     {
       if (out)
       {
         (*out) << "Checking application of " << Expr(hd) << std::endl;
-        if (isQuote)
-        {
-          (*out) << "Unexpected child #" << i << std::endl;
-          (*out) << "  Term: " << Expr(children[i]) << std::endl;
-          (*out) << "  Expected pattern: ";
-        }
-        else
-        {
-          (*out) << "Unexpected type of child #" << i << std::endl;
-          (*out) << "  Term: " << Expr(children[i]) << std::endl;
-          (*out) << "  Has type: " << Expr(child) << std::endl;
-          (*out) << "  Expected type: ";
-        }
+        (*out) << "Unexpected type of child #" << i << std::endl;
+        (*out) << "  Term: " << Expr(children[i]) << std::endl;
+        (*out) << "  Has type: " << Expr(child) << std::endl;
+        (*out) << "  Expected type: ";
         (*out) << Expr(hdt) << std::endl;
         (*out) << "  Context " << ctx << std::endl;
       }
