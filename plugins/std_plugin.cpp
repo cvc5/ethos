0f46a2d840cc291ee4bfdb478c75cfce73fcfc2c
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2026-08-20 07:53:59 -0500

    Add CI infrastructure for plugins, generic base class for plugin (#218)
    Adds std_plugin, which contains utilities that are common to forthcoming
    plugins.
    
    Also adds a CI check to ethos to ensure that plugins compile.

---
+++ plugins/std_plugin.cpp
@@ -0,0 +1,309 @@
+/******************************************************************************
+ * This file is part of the ethos project.
+ *
+ * Copyright (c) 2023-2024 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ ******************************************************************************/
+
+#include "std_plugin.h"
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <cerrno>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include "state.h"
+
+namespace ethos {
+
+namespace {
+
+std::string normalizeDirectory(std::string path)
+{
+  if (path.empty())
+  {
+    return path;
+  }
+  if (path.back() != '/')
+  {
+    path.push_back('/');
+  }
+  return path;
+}
+
+std::string getParentDirectory(const std::string& path)
+{
+  std::string::size_type pos = path.find_last_of('/');
+  if (pos == std::string::npos)
+  {
+    return std::string();
+  }
+  if (pos == 0)
+  {
+    return "/";
+  }
+  return path.substr(0, pos);
+}
+
+void ensureDirectoryExists(const std::string& path)
+{
+  if (path.empty() || path == ".")
+  {
+    return;
+  }
+
+  std::stringstream current;
+  std::size_t start = 0;
+  if (path[0] == '/')
+  {
+    current << "/";
+    start = 1;
+  }
+
+  while (start < path.size())
+  {
+    std::size_t end = path.find('/', start);
+    std::string part = end == std::string::npos
+                           ? path.substr(start)
+                           : path.substr(start, end - start);
+    start = end == std::string::npos ? path.size() : end + 1;
+    if (part.empty() || part == ".")
+    {
+      continue;
+    }
+    if (part == "..")
+    {
+      if (current.tellp() != std::streampos(0) && current.str().back() != '/')
+      {
+        current << "/";
+      }
+      current << "..";
+      continue;
+    }
+    if (current.tellp() != std::streampos(0) && current.str().back() != '/')
+    {
+      current << "/";
+    }
+    current << part;
+    std::string currPath = current.str();
+    struct stat sb;
+    if (stat(currPath.c_str(), &sb) == 0)
+    {
+      if (!S_ISDIR(sb.st_mode))
+      {
+        EO_FATAL() << "StdPlugin: path exists and is not a directory: "
+                   << currPath;
+      }
+      continue;
+    }
+    if (mkdir(currPath.c_str(), 0755) != 0 && errno != EEXIST)
+    {
+      EO_FATAL() << "StdPlugin: failed to create directory " << currPath
+                 << ", errno = " << errno;
+    }
+  }
+}
+
+}  // namespace
+
+std::string StdPlugin::initializePluginPath()
+{
+  const char* envPath = std::getenv("ETHOS_PLUGIN_ROOT");
+  if (envPath != nullptr && envPath[0] != '\0')
+  {
+    return normalizeDirectory(envPath);
+  }
+#ifdef ETHOS_PLUGIN_SOURCE_DIR
+  return normalizeDirectory(ETHOS_PLUGIN_SOURCE_DIR);
+#else
+  return std::string();
+#endif
+}
+
+std::string StdPlugin::initializePluginOutputPath()
+{
+  const char* envPath = std::getenv("ETHOS_PLUGIN_OUTPUT_DIR");
+  if (envPath != nullptr && envPath[0] != '\0')
+  {
+    return normalizeDirectory(envPath);
+  }
+#ifdef ETHOS_PLUGIN_OUTPUT_DIR
+  return normalizeDirectory(ETHOS_PLUGIN_OUTPUT_DIR);
+#else
+  return std::string();
+#endif
+}
+
+std::string StdPlugin::getResourcePath(const std::string& relativePath)
+{
+  return s_plugin_path + relativePath;
+}
+
+std::string StdPlugin::getOutputPath(const std::string& relativePath)
+{
+  std::string path = s_plugin_output_path + relativePath;
+  ensureDirectoryExists(getParentDirectory(path));
+  return path;
+}
+
+void StdPlugin::copyResourceToOutput(const std::string& relativePath)
+{
+  std::ifstream in(getResourcePath(relativePath));
+  if (!in.is_open())
+  {
+    EO_FATAL() << "StdPlugin: failed to open resource " << relativePath;
+  }
+  std::string outPath = getOutputPath(relativePath);
+  std::ofstream out(outPath);
+  if (!out.is_open())
+  {
+    EO_FATAL() << "StdPlugin: failed to open output " << outPath;
+  }
+  out << in.rdbuf();
+}
+
+std::string StdPlugin::s_plugin_path = StdPlugin::initializePluginPath();
+std::string StdPlugin::s_plugin_output_path =
+    StdPlugin::initializePluginOutputPath();
+
+StdPlugin::StdPlugin(State& s) : d_state(s), d_tc(s.getTypeChecker())
+{
+  d_typeVarCounter = 0;
+  if (s_plugin_path.empty())
+  {
+    EO_FATAL() << "StdPlugin: unable to determine the plugin resource root";
+  }
+  if (s_plugin_output_path.empty())
+  {
+    EO_FATAL() << "StdPlugin: unable to determine the plugin output root";
+  }
+}
+
+StdPlugin::~StdPlugin() {}
+
+std::vector<Expr> StdPlugin::getSubtermsKind(Kind k, const Expr& t)
+{
+  std::vector<Expr> ret;
+  std::set<Expr> visited;
+  std::vector<Expr> toVisit;
+  toVisit.push_back(t);
+  Expr cur;
+  do
+  {
+    cur = toVisit.back();
+    toVisit.pop_back();
+    if (visited.find(cur) != visited.end())
+    {
+      continue;
+    }
+    visited.insert(cur);
+    if (cur.getKind() == k)
+    {
+      ret.push_back(cur);
+    }
+    for (size_t i = 0, nchild = cur.getNumChildren(); i < nchild; i++)
+    {
+      toVisit.push_back(cur[i]);
+    }
+  } while (!toVisit.empty());
+  return ret;
+}
+
+bool StdPlugin::hasSubterm(const Expr& t, const Expr& s)
+{
+  std::unordered_set<const ExprValue*> visited;
+  std::vector<Expr> toVisit;
+  toVisit.push_back(t);
+  Expr cur;
+  while (!toVisit.empty())
+  {
+    cur = toVisit.back();
+    toVisit.pop_back();
+    const ExprValue* cv = cur.getValue();
+    if (visited.find(cv) != visited.end())
+    {
+      continue;
+    }
+    visited.insert(cv);
+    if (cur == s)
+    {
+      return true;
+    }
+    for (size_t i = 0, nchildren = cur.getNumChildren(); i < nchildren; i++)
+    {
+      toVisit.push_back(cur[i]);
+    }
+  }
+  return false;
+}
+
+Expr StdPlugin::allocateTypeVariable()
+{
+  d_typeVarCounter++;
+  std::stringstream ss;
+  ss << "$eoT_" << d_typeVarCounter;
+  return d_state.mkSymbol(Kind::PARAM, ss.str(), d_state.mkType());
+}
+
+Expr StdPlugin::getGroundTermForLiteralKind(Kind k)
+{
+  Expr gt;
+  switch (k)
+  {
+    case Kind::NUMERAL: gt = d_state.mkLiteral(k, "0"); break;
+    case Kind::RATIONAL: gt = d_state.mkLiteral(k, "0/1"); break;
+    case Kind::BINARY: gt = d_state.mkLiteral(k, "0"); break;
+    case Kind::STRING: gt = d_state.mkLiteral(k, ""); break;
+    default: break;
+  }
+  return gt;
+}
+
+std::string StdPlugin::literalKindToString(Kind k)
+{
+  std::stringstream ss;
+  switch (k)
+  {
+    case Kind::NUMERAL: ss << "<numeral>"; break;
+    case Kind::RATIONAL: ss << "<rational>"; break;
+    case Kind::BINARY: ss << "<binary>"; break;
+    case Kind::STRING: ss << "<string>"; break;
+    case Kind::DECIMAL: ss << "<decimal>"; break;
+    case Kind::HEXADECIMAL: ss << "<hexadecimal>"; break;
+    default: EO_FATAL() << "Unknown literal type rule" << k << std::endl; break;
+  }
+  return ss.str();
+}
+
+void StdPlugin::replace(std::string& txt,
+                        const std::string& tag,
+                        const std::string& replacement)
+{
+  auto pos = txt.find(tag);
+  if (pos != std::string::npos)
+  {
+    txt.replace(pos, tag.length(), replacement);
+  }
+}
+
+std::string StdPlugin::replace_all(std::string str,
+                                   const std::string& from,
+                                   const std::string& to)
+{
+  if (from.empty()) return str;  // avoid infinite loop
+
+  std::size_t pos = 0;
+  while ((pos = str.find(from, pos)) != std::string::npos)
+  {
+    str.replace(pos, from.length(), to);
+    pos += to.length();  // move past the replacement
+  }
+  return str;
+}
+}  // namespace ethos
---
+++ plugins/std_plugin.h
@@ -0,0 +1,87 @@
+/******************************************************************************
+ * This file is part of the ethos project.
+ *
+ * Copyright (c) 2023-2024 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ ******************************************************************************/
+#ifndef STD_PLUGIN_H
+#define STD_PLUGIN_H
+
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+
+#include "plugin.h"
+#include "state.h"
+#include "type_checker.h"
+
+namespace ethos {
+
+class State;
+class TypeChecker;
+
+/**
+ * The purpose of this plugin is to do things that are common to multiple
+ * plugins. For example, tracking the dependencies for literal types.
+ */
+class StdPlugin : public Plugin
+{
+ public:
+  StdPlugin(State& s);
+  ~StdPlugin();
+
+ protected:
+  /** Allocate a fresh type variable */
+  Expr allocateTypeVariable();
+  /** Get ground term for kind */
+  Expr getGroundTermForLiteralKind(Kind k);
+  /** Does t have subterm s? */
+  static bool hasSubterm(const Expr& t, const Expr& s);
+  /** Get the subterms of a kind */
+  std::vector<Expr> getSubtermsKind(Kind k, const Expr& t);
+  /** Get the name of the literal kind k, e.g. "<numeral>" for NUMERAL. */
+  static std::string literalKindToString(Kind k);
+  /** Replace the first occurrence of tag in txt by replacement. */
+  static void replace(std::string& txt,
+                      const std::string& tag,
+                      const std::string& replacement);
+  /** Replace all occurrences of from in str by to and return the result. */
+  static std::string replace_all(std::string str,
+                                 const std::string& from,
+                                 const std::string& to);
+  /** the state */
+  State& d_state;
+  /** the type checker */
+  TypeChecker& d_tc;
+  /** type variable counter */
+  size_t d_typeVarCounter;
+  /**
+   * The root directory containing the plugin resource files, i.e. the
+   * templates a plugin reads. Taken from ETHOS_PLUGIN_ROOT if set, else from
+   * the compile-time definition ETHOS_PLUGIN_SOURCE_DIR supplied by the build.
+   */
+  static std::string s_plugin_path;
+  /**
+   * The root directory where generated artifacts are written. Taken from
+   * ETHOS_PLUGIN_OUTPUT_DIR if set, else from the compile-time definition of
+   * the same name supplied by the build.
+   */
+  static std::string s_plugin_output_path;
+  /** Determine the root containing the plugin resources */
+  static std::string initializePluginPath();
+  /** Determine the default directory for generated artifacts */
+  static std::string initializePluginOutputPath();
+  /** Get a path relative to the source resource root */
+  static std::string getResourcePath(const std::string& relativePath);
+  /** Get a path relative to the generated output root */
+  static std::string getOutputPath(const std::string& relativePath);
+  /** Copy a static resource into the generated output tree */
+  static void copyResourceToOutput(const std::string& relativePath);
+};
+
+}  // namespace ethos
+
+#endif /* STD_PLUGIN_H */
